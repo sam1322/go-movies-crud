0587d832bf9059778cb4d70f9c741e379dafe970
Sam <sriram.pant5@gmail.com>
2024-01-27 01:10:20 +0530

    removing comments

--- server-old/main.go
+++ server-old/main.go
@@ -125,18 +125,77 @@ func main() {
 
 	movies = append(movies, Movie{ID: "2", Isbn: "345235", Title: "The Lost World: Jurassic Park", Img: "https://raisingchildren.net.au/__data/assets/image/0019/51355/jurassic-park-ii-the-lost-world.jpg", Director: &Director{Firstname: "Steven", Lastname: "Spielberg"}})
 
+	// router.Use(func(next http.Handler) http.Handler {
+	// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 		// Set CORS headers
+	// 		// log.Printf("Request received from %v\n", r.RemoteAddr)
+	// 		log.Println("Request received from ", r.RemoteAddr)
+
+	// 		w.Header().Set("Access-Control-Allow-Origin", "*") // Update with your frontend's origin
+	// 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, HEAD, DELETE, OPTIONS")
+	// 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// 		if r.Method == "OPTIONS" {
+	// 			w.WriteHeader(http.StatusNoContent)
+	// 			return
+	// 		}
+	// 		// Continue with the request
+	// 		next.ServeHTTP(w, r)
+	// 	})
+	// })
+
 	router.HandleFunc("/movies", getMovies).Methods("GET")
 	router.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	router.HandleFunc("/movies/create", createMovie).Methods("POST")
 	router.HandleFunc("/movies/update/{id}", updateMovie).Methods("POST")
 	router.HandleFunc("/movies/delete/{id}", deleteMovies).Methods("DELETE")
 	http.Handle("/", router)
+
+	// credentials := handlers.AllowCredentials()
+
+	// header := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	// methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"})
+	// origins := handlers.AllowedOrigins([]string{"*"})
+
 	fmt.Println("Starting server at port 8080")
+
+	// c := cors.New(cors.Options{
+	// 	AllowedOrigins:   []string{"http://localhost:3000"},
+	// 	AllowCredentials: true,
+	// })
+
+	// handler := c.Handler(router)
+	// log.Fatal(http.ListenAndServe(":8080", handler))
+
+	// credentials := handlers.AllowCredentials()
+	// methods := handlers.AllowedMethods([]string{"POST"})
+	// // ttl := handlers.MaxAge(3600)
+	// origins := handlers.AllowedOrigins([]string{"http://localhost:3000"})
+	// log.Fatal(http.ListenAndServe(":8080", handlers.CORS(credentials, methods, origins)(router)))
+
+	// handlers.AllowedOrigins([]string{"http://0.0.0.0:3000"})
+
+	// log.Fatal(http.ListenAndServe(":8080", router))
 
 	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"})
 	originsOk := handlers.AllowedOrigins([]string{"http://localhost:3000"})
 
+	// router.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+	// 	log.Println("Up and running!")
+	// })
+
+	// log.Fatal(http.ListenAndServe(":8080", handlers.CORS(
+	// 	// handlers.AllowedMethods([]string{"GET", "POST", "PUT"}),
+	// 	handlers.AllowedHeaders([]string{"Accept", "Accept-Language", "Content-Type", "Content-Language", "Origin"}),
+	// 	// handlers.AllowedOrigins([]string{"*"}),
+	// )(router)))
 	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
+
+	// http.ListenAndServe(":8080", handlers.CORS()(router))
+	// http.ListenAndServe(":8080", handlers.CORS(origins, header, methods)(router))
+	// http.ListenAndServe(":8080", handlers.CORS(methods, origins, header)(router))
+	// http.ListenAndServe(":8080", handlers.CORS(header, methods, origins)(router))
+	// http.ListenAndServe(":8080", handlers.CORS(credentials)(router))
 
 }
