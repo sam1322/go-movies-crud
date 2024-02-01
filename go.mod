484b58d66d8fa9092f7cdee905915197fa2ec45c
Sam <sriram.pant5@gmail.com>
2024-02-01 08:28:01 +0530

    chat bot server using websockets

--- go.mod
+++ go.mod
@@ -10,5 +10,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/rs/cors v1.10.1 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	gorm.io/gorm v1.25.5 // indirect
+	nhooyr.io/websocket v1.8.10 // indirect
 )
--- new-server/main.go
+++ new-server/main.go
@@ -1,31 +1,71 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"net/http"
+
+// 	"github.com/gorilla/mux"
+// )
+
+// func PrintHello(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Println("Hellow")
+// 	fmt.Fprintf(w, "Hello you've requested: %s\n", r.URL.Path)
+// }
+
+// func PrintHelloWorld(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Println("Hello World")
+// 	vars := mux.Vars(r)
+// 	title := vars["title"]
+// 	fmt.Fprintf(w, "You have requested the following title => %s\n", title)
+// 	// fmt.Fprintf(w, "Hello you've requested: %s\n", r.URL.Path)
+// }
+
+// func main() {
+// 	// http.HandleFunc("/hello", PrintHello)
+// 	// http.ListenAndServe(":8080", nil)
+
+//		r := mux.NewRouter()
+//		r.HandleFunc("/hello", PrintHello)
+//		r.HandleFunc("/hello/{title}", PrintHelloWorld)
+//		http.ListenAndServe(":8080", r)
+//	}
 package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/gorilla/mux"
+	"time"
 )
 
-func PrintHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Hellow")
-	fmt.Fprintf(w, "Hello you've requested: %s\n", r.URL.Path)
-}
-
-func PrintHelloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Hello World")
-	vars := mux.Vars(r)
-	title := vars["title"]
-	fmt.Fprintf(w, "You have requested the following title => %s\n", title)
-	// fmt.Fprintf(w, "Hello you've requested: %s\n", r.URL.Path)
-}
-
 func main() {
-	// http.HandleFunc("/hello", PrintHello)
-	// http.ListenAndServe(":8080", nil)
+	pingChan := make(chan int, 1)
+	pongChan := make(chan int, 1)
 
-	r := mux.NewRouter()
-	r.HandleFunc("/hello", PrintHello)
-	r.HandleFunc("/hello/{title}", PrintHelloWorld)
-	http.ListenAndServe(":8080", r)
+	go ping(pingChan, pongChan)
+	go pong(pongChan, pingChan)
+
+	pingChan <- 1
+
+	select {}
+}
+
+func ping(pingChan <-chan int, pongChan chan<- int) {
+	for {
+		ball := <-pingChan
+
+		fmt.Println("Ping", ball)
+		time.Sleep(1 * time.Second)
+
+		pongChan <- ball + 1
+	}
+}
+
+func pong(pongChan <-chan int, pingChan chan<- int) {
+	for {
+		ball := <-pongChan
+
+		fmt.Println("Pong", ball)
+		time.Sleep(1 * time.Second)
+
+		pingChan <- ball + 1
+	}
 }
